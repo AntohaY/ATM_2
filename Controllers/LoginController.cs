398c4cb03cb5c08a8be8a8164405dd8551f80ec6
AntohaY <antonyakovenko2401@gmail.com>
2018-05-25 12:31:41 +0300

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using ATM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ATM.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                Card card = null;
+
+                using(BankDBEntities db = new BankDBEntities())
+                {
+                    card = db.Card.FirstOrDefault(c => c.CardID == model.CardId && c.PinCode == model.PinCode);
+                }
+                if (card != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.CardId, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+                return View(model);
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATM.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string CardId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PinCode { get; set; }
+    }
+}
